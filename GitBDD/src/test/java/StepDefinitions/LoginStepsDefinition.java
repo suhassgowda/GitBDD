de00eaf3b5ef937d6645d97f276ad9bf2e3ea042
Suhas S <suhassgowda123@gmail.com>
2024-11-19 00:11:02 +0530

    New Change Print Statement PR

--- GitBDD/src/test/java/StepDefinitions/LoginStepsDefinition.java
+++ GitBDD/src/test/java/StepDefinitions/LoginStepsDefinition.java
@@ -55,6 +55,7 @@ public class LoginStepsDefinition {
 	@And("Close the Browser")
 	public void close_the_browser() {
 	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Completed");
 	    driver.quit();
 	}
 
